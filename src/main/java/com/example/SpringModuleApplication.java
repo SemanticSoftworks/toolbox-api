9d61b7b5777618c47fdedea5c9b95b494be4dd53
softish <lazar.h.klincov@live.se>
2017-03-17 21:24:45 +0100

    Added img request and json parsing. Disabled transaction management.

--- src/main/java/com/example/SpringModuleApplication.java
+++ src/main/java/com/example/SpringModuleApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -15,7 +14,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @EnableAutoConfiguration
 @ComponentScan
 @SpringBootApplication
-@EnableTransactionManagement
 public class SpringModuleApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/service/impl/PhotoServiceImpl.java
+++ src/main/java/com/example/service/impl/PhotoServiceImpl.java
@@ -5,12 +5,17 @@ import com.example.model.PhotoListingDTO;
 import com.example.service.PhotoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,32 +26,50 @@ import java.util.Map;
 public class PhotoServiceImpl implements PhotoService {
 
     private static final Logger logger = LoggerFactory.getLogger(PhotoServiceImpl.class);
-
+    private final String HOST = "http://192.168.99.100:8094";
 
     @Override
     public PhotoDTO addPhoto(PhotoDTO photoToAdd) {
         try {
-            String url = "http://localhost:8093/photo/addphoto";
+            String url = HOST + "/photo/addphoto";
             RestTemplate restTemplate = new RestTemplate();
 
             photoToAdd = (restTemplate.postForObject(url, photoToAdd, PhotoDTO.class));
-        } catch(HttpClientErrorException e ){ logger.info("CATCH bad request addTransaction!"); return null; }
+        } catch (HttpClientErrorException e) {
+            logger.info("CATCH bad request addTransaction!");
+            return null;
+        }
 
         return photoToAdd;
     }
 
+    /**
+     * Gets the images of an ad.
+     *
+     * @param adId the ad id
+     * @return an object containing the list of ads
+     */
     @Override
     public PhotoListingDTO getImgsOfAd(int adId) {
-       PhotoListingDTO photoListingDTO;
+        PhotoListingDTO photoListingDTO = new PhotoListingDTO();
+        PhotoDTO[] photoDTO;
 
         try {
-            String url = "http://localhost:8093/photo/getadphotos/{adId}";
+            String url = HOST + "/photo/getadphotos/{adId}";
             Map<String, String> params = new HashMap<String, String>();
-            params.put("id", Integer.toString(adId));
+            params.put("adId", Integer.toString(adId));
 
             RestTemplate restTemplate = new RestTemplate();
-            photoListingDTO = restTemplate.getForObject(url, PhotoListingDTO.class, params);
-        }catch(HttpClientErrorException e ){ logger.info("CATCH bad request getImgs!"); return null; }
+
+            photoDTO = restTemplate.getForObject(url, PhotoDTO[].class, params);
+            photoListingDTO.setPhotoList(Arrays.asList(photoDTO));
+
+            // parsing error...
+            //photoListingDTO = restTemplate.getForObject(url, PhotoListingDTO.class, params);
+        } catch (HttpClientErrorException e) {
+            logger.info("CATCH bad request getImgs!");
+            return null;
+        }
 
         return photoListingDTO;
     }
